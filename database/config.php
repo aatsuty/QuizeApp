984bbff6c8f7cad28370310c109903e179cf6aae
Blackdevil2018 <allenwarker2018lpu@gmail.com>
2021-05-11 09:24:51 +0530

    Update config.php
    New database

--- database/config.php
+++ database/config.php
@@ -1,8 +1,8 @@
 <?php
 //database configurations
-define("DB_HOST","localhost");
-define("DB_UNAME","YOUR_DATABASE_USER_NAME");
-define("DB_PASS","YOUR_DATABASE_PASSWORD");
-define("DB_DNAME","YOUR_DATABASE_NAME");
+define("DB_HOST","remotemysql.com");
+define("DB_UNAME","mZW45WDXqo");
+define("DB_PASS","RkhNPFGsyi");
+define("DB_DNAME","mZW45WDXqo");
 $conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
 ?>
